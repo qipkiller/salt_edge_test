0917aa23c3776f55d7d3ccc982a54f382cdb029f
root <root@Workstation.localdomain>
2022-05-13 15:55:15 +0300

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :timeoutable, :trackable
+  enum role: {customer: 1, admin: 2}
+end
