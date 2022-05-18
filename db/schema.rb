d7f8c022c7a02748de7d551f1228188d1e403710
qipkiller <a.sarev00@gmail.com>
2022-05-18 11:57:40 +0300

    fix turbolinks, chema, connection link

--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_05_16_110947) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "connections", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "remote_id"
+    t.integer "user_id", null: false
+    t.string "remote_id"
     t.boolean "arhived", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -38,7 +35,7 @@ ActiveRecord::Schema.define(version: 2022_05_16_110947) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "role", default: 1
-    t.bigint "customer_id"
+    t.string "customer_id"
     t.string "secret"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
